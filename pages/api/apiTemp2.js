925bd0fd207f8cfbc68b7609b96d57d86849f04d
JohnE10 <csworg@gmail.com>
2023-05-30 20:41:01 +0100

    Update

--- pages/api/apiTemp2.js
+++ pages/api/apiTemp2.js
@@ -1,87 +1,52 @@
 import { split } from 'postcss/lib/list';
-import { fileTree } from '../../utils/helpers';
+import { fileTree, isAbsoluteURL } from '../../utils/helpers';
 import { chooseDirectory, createDirectory, createDirectoryAndSaveFile, deleteDirectoryContents, fileOrDirectory, listFilesInDirectory } from './backEndHelpers';
 
 const ApiTemp2 = async (req, res) => {
 
 	const fs = require('fs');
 	const path = require('path');
-	const postcss = require('postcss');
-	const cssnano = require('cssnano');
-	const valueParser = require('postcss-value-parser');
-	const csstree = require('css-tree');
+  
+	const filePath = 'C:/Users/jetto/OneDrive/Desktop/Files/Coding-ASUS/WP Migration Campaign/HTtrack/HTTrack_dumasfamilydentistry/dumasfamilydentistry/dumasfamilydentistry/index.html';
 
+	const fileContent = 'Temp text';
 
+	const html = fs.readFileSync(filePath, { encoding: 'utf8' });
 
-	function addSemicolonsToCSS() {
-		// const cssFilePath = path.join(__dirname, 'path_to_your_css_file.css');
+	if (html) {
+		// load cheerio
+		let $ = cheerio.load(html);
 
-		const cssFilePath = 'C:/Users/jetto/OneDrive/Desktop/Files/Coding-ASUS/WP Migration Campaign/smilingfacesnola/pages/stylesheets.css';
+		let aTags = $('body').find('a');
 
-		// Read the CSS file
-		let css = fs.readFileSync(cssFilePath, 'utf8');
-
-		css = css.replaceAll('}', ';}');
-		css = css.replaceAll(';;', ';');
-
-		console.log(css);
-
-		// // split css
-		// const splitCss = css.split('.');
-		// console.log(splitCss);
-
-		// const modifiedSplicScc = splitCss.map((ele) => {
-
-		// 		if (ele.includes('{') && ele.includes('}')) {
-		// 			return ele;
-		// 			// // if(ele == 'undefined' || ele == '') {
-		// 			// // 	return '';
-		// 			// // }
-		// 			// if (!ele.includes(';}')) {
-		// 			// 	ele = ele.replace('}', ';}');
-		// 			// 	return ele;
-		// 			// }
-		// 			// // else {
-		// 			// // 	return 'nothing';
-		// 			// // }
-		// 		}
-		// });
-
-		// console.log(modifiedSplicScc);
+		            // modify a tag hrefs to .js from .html
+					aTags.each((i, el) => {
+						if ($(el).attr('href')) {
+							let temp = $(el).attr('href').trim();
+							if (!isAbsoluteURL(temp)) {
+								if (temp.includes('.html')) {
+									if (fileNameFromUrl(temp).parentDirectory == '') {
+										$(el).attr('href', '/');
+									}
+									else {
+										const filename = fileNameFromUrl(temp).fileName;
+										temp = temp.replace(filename, '');
+										$(el).attr('href', temp);
+									}
+								}
+		
+							}
+						}
+					});
+					
 	}
 
-	addSemicolonsToCSS();
-
-	// async function optimizeAndMinifyCSS() {
-	// 	// const cssFilePath = path.join(__dirname, 'temp.css');
-	// 	const cssFilePath = 'C:/Users/jetto/OneDrive/Desktop/Files/Coding-ASUS/WP Migration Campaign/smilingfacesnola/pages/stylesheets.css';
-
-	// 	// Read the CSS file
-	// 	const css = fs.readFileSync(cssFilePath, 'utf8');
-
-	// 	// Process the CSS using PostCSS and cssnano
-	// 	const processedCss = await postcss([cssnano()]).process(css, {
-	// 		from: cssFilePath,
-	// 		to: cssFilePath
-	// 	});
-
-	// 	// Get the optimized and minified CSS
-	// 	const optimizedCss = processedCss.css;
-
-	// 	// Write the optimized CSS back to the file
-	// 	fs.writeFileSync(cssFilePath, optimizedCss, 'utf8');
-
-	// 	// console.log('CSS optimized and minified.');
-	// 	console.log(optimizedCss);
-	// }
-
-	// optimizeAndMinifyCSS();
 
 
 
+	const temp = isAbsoluteURL(filePath);
 
-
-	return res.json('Done');
+	return res.json(temp);
 
 };
 
